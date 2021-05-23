77c7f2aeae75a8cac2bb34b301676ca308463635
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-05-23 13:07:36 -0300

    alter memcpy 2

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,21 +6,27 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 20:34:33 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/05/23 13:04:50 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/05/23 13:07:28 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void *ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
+	char *dst1;
+	char *src1;
 
 	i = 0;
-	while (i < n)
-	{
-		((char *)dest)[i] = ((char *)src)[i];
-		i++;
-	}
-	return (dest);
+	dst1 = dst;
+	src1 = src;
+    if (dst1 == src1)
+    	return (dst1);
+	while(i < n)
+    {
+			dst1[i] = src1[i];
+            i++;
+    }
+	return (dst);
 }
