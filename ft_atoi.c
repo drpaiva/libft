56f50c0afb97defb9242a4109571973f49254d27
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-05-23 20:49:28 -0300

    add testefor debug TMP

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,12 +6,12 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 06:49:31 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/05/23 20:41:17 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/05/23 20:48:51 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
+// #include "libft.h"
+#include <stddef.h>
 int	ft_atoi(const char *str)
 {
 	size_t	res;
@@ -21,7 +21,7 @@ int	ft_atoi(const char *str)
 	res = 0;
 	sign = 0;
 	i = 0;
-	while (str[i] < 42)
+	while (str[i] < ')')
 		i++;
 	while (str[i] == '+' || str[i] == '-')
 	{
@@ -38,4 +38,16 @@ int	ft_atoi(const char *str)
 		return (res);
 	else
 		return (-res);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	char	*n = "\t\v\f\r\n \f-06050";
+
+	int		i1 = atoi(n);
+	int		i2 = ft_atoi(n);
+	if (i1 == i2)
+		printf("deu bom | %d | %d |\n", i1, i2);
 }
