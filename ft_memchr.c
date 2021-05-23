ce7cb8819713778a3df90bc7f6d66a9dc76bba59
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-05-23 17:33:53 -0300

    alter memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 20:57:11 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/05/23 17:19:22 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/05/23 17:33:41 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,17 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char	*news;
 	size_t i;
 
 	i = 0;
-	news = (unsigned char *)s;
-	while (n >= 0 || *news)
+	if (!((unsigned char *)s))
+		return (0);
+	while (i < n)
 	{
-		if (news[i] == c)
-			return (&news[i]);
-		n--;
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+		{
+			return ((void *)&((unsigned char *)s)[i]);
+		}
 		i++;
 	}
 	return (0);
