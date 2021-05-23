7adb5ec1c624f28ef3c532b1a78fcfac5e42d5b6
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-05-23 14:15:22 -0300

    ajust file

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 20:57:11 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/05/23 13:44:25 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/05/23 14:14:43 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	size_t i;
 
 	i = 0;
+	if (!((unsigned char *)s))
+		return (0);
 	while (i < n)
 	{
 		if (((unsigned char *)s)[i] == (unsigned char)c)
