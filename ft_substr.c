8ee2f6a7226b68b3fe850afbf7b6a30ec5bb1d84
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-05-24 00:12:27 -0300

    alter substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 23:02:07 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/05/23 23:52:20 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/05/24 00:11:42 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,27 @@
 char *ft_substr(char const *s, size_t start, size_t len)
 {
 	char *news;
-	size_t t;
+	// size_t t;
 
-	s+start;
+	// *s + start;
 	if (!len)
 		return (0);
 	news = malloc(sizeof(char) * len);
 	if (!news)
 		return (0);
-	t = ft_strlcpy(*news, s, len);
+	ft_strlcpy(news, s + start, len);
 	return (news);
 }
+// #include <stdio.h>
+// int main()
+// {
+// 	char	*str = "i just want this part #############";
+// 	size_t	size = 22;
+
+// 	char	*ret = ft_substr(str, 0, size);
+// 	printf("%s",ret);
+// 	// if (!strncmp(ret, str, size)) {
+// 	// 	free(ret);
+// 	// }
+// 	return 0;
+// }
