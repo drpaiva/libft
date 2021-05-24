0e20e7cb439d2bb642e978b2eb320346c4ac7e74
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-05-23 23:52:50 -0300

    add

--- ft_substr.c
+++ ft_substr.c
@@ -6,13 +6,13 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 23:02:07 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/05/23 23:44:18 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/05/23 23:52:20 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char *ft_substr(char const *s, size_t start, size_t len)
 {
 	char *news;
 	size_t t;
