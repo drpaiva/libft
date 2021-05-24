22e6d53347e1091086ead912a5d283dff5be66b3
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-05-24 00:30:12 -0300

    alter sub str

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 23:02:07 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/05/24 00:23:26 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/05/24 00:30:06 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!len)
 		return (0);
-	news = malloc(sizeof(char) * len);
+	news = malloc(sizeof(char) * len + 1);
 	if (!news)
 		return (0);
-	ft_strlcpy(news, s + start, len);
+	ft_strlcpy(news, s + start, len + 1);
 	return (news);
 }
