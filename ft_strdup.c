ad84cb1255659d71f21ab647aa55c9796ed01b13
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-05-25 20:41:26 -0300

    add new line ft_strjoin for makefile

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/22 16:14:01 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/05/22 18:01:25 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/05/25 20:41:13 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ char	*ft_strdup(const char *s1)
 	len = ft_strlen(s1);
 	sizes1 = sizeof(char) * len + 1;
 	news1 = (char *)malloc(sizes1);
+	if (!news1)
+		return (0);
 	ft_bzero(news1, sizes1);
 	return ((char *)ft_memcpy(news1, s1, sizes1));
 }
