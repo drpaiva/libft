b2c678517c4df593d5526975cf35c6d5907b4d5e
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-05-26 07:42:06 -0300

    adjust norminette of the function strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 03:13:42 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/05/26 07:21:06 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/05/26 07:41:26 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char *res;
-	int i;
-	int f;
+	char	*res;
+	int		i;
+	int		f;
 
 	i = 0;
-	if (!s1)
+	f = ft_strlen(s1) - 1;
+	if (f <= 0)
 		return (0);
 	while (s1[i] && ft_strchr(set, s1[i]))
 		s1++;
-	f = ft_strlen(s1) - 1;
 	while (s1[f] && ft_strchr(set, s1[f]))
 		f--;
 	res = malloc(sizeof(char) * (f - i + 2));
