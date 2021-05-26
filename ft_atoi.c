ba550883de97a997fe25f87a0b7f32ab693790ff
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-05-26 02:57:18 -0300

    ajustando o atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 06:49:31 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/05/26 02:34:33 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/05/26 02:56:15 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@ int	ft_atoi(const char *str)
 	size_t	res;
 	size_t	sign;
 	size_t	i;
+	size_t	s;
 
 	res = 0;
 	sign = 0;
 	i = 0;
+	s = 0;
 	while (str[i] <= 13 && str[i] >= 9 && str[i] != 32)
 		i++;
 	while (str[i] == '+' || str[i] == '-')
@@ -28,13 +30,11 @@ int	ft_atoi(const char *str)
 		if (str[i] == '-')
 			sign++;
 		i++;
+		s++;
 	}
 	while (str[i] && (str[i] >= '0') && (str[i] <= '9'))
-	{
-		res = res * 10 + str[i] - '0';
-		i++;
-	}
-	if (sign > 1)
+		res = res * 10 + str[i++] - '0';
+	if (s > 1)
 		return (0);
 	if ((sign % 2) == 0)
 		return (res);
