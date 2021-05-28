a59fe0c8a80f49279857e7505ce35c2c57f32c29
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-05-28 16:39:39 -0300

    ajust substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 23:02:07 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/05/28 16:32:27 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/05/28 16:38:56 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	unsigned int	c;
 
 	c = ft_strlen(s);
-	if (start > c)
+	if (!len || !s || start > c)
 		return (news = (char *)ft_calloc(sizeof(char), 1));
-	if (!len || !s)
-		return (news = (char *)ft_calloc(sizeof(char), 1));
-	news = (char *)ft_calloc(sizeof(char), len);
+	news = (char *)ft_calloc(sizeof(char), len + 1);
 	if (!news)
 		return (NULL);
 	ft_strlcpy(news, s + start, len + 1);
