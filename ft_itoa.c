07c1aa8691c69e4ca1725e2bf6ec56befbf65054
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-05-31 19:05:01 -0300

    add itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,21 +6,16 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/29 14:10:08 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/05/31 18:47:01 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/05/31 19:04:52 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-static char *ft_vetrev()
-{
-
-}
 
 char	*ft_itoa(int n)
 {
 	char	*res;
 	int		i;
-	int		d;
 	int		val;
 
 	i = 0;
