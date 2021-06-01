47d14b64b177373da4ad68ca68e03fd072f09b38
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-06-01 20:03:54 -0300

    add lib unistd for libft.h

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 19:58:07 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/06/01 19:59:37 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/06/01 20:03:45 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 void	ft_putchar_fd(char c, int fd)
 {
-	write(fd, c, 1);
+	write(fd, &c, 1);
 }
