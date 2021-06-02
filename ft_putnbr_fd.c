4a59b0a89b7307270598350e96f307486a66eb80
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-06-01 22:00:12 -0300

    add function putnbr_fd 2

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,49 +6,81 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 20:22:48 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/06/01 21:49:02 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/06/01 21:59:58 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	ft_lendec(int n)
+static void	calc_print(long int qto_d, long int nb2, int fd)
 {
-	size_t	i;
-	size_t	res;
+	long int	el;
+	long int	resultado;
+	long int	conta;
 
-	i = 0;
-	res = n;
-	if (n <= 0)
+	while (qto_d > 0)
 	{
-		res = res * -1;
-		i++;
+		el = v_div(qto_d);
+		conta = nb2 / el;
+		resultado = conta + '0';
+		write(fd, &resultado, 1);
+		nb2 = nb2 - el * conta;
+		qto_d--;
 	}
-	while (res)
+}
+
+static long int	v_div(long int num)
+{
+	long int	count;
+	long int	result;
+
+	result = 10;
+	count = 1;
+	while (count < num)
 	{
-		res = res / 10;
-		i++;
+		result = result * 10;
+		count++;
 	}
-	return (i);
+	return (result / 10);
+}
+
+static long int	qto_casas(long int x)
+{
+	long int	casas;
+
+	casas = 0;
+	while (x > 0)
+	{
+		x = x / 10;
+		casas++;
+	}
+	return (casas);
+}
+
+static long int	ft_is_negative(long int n)
+{
+	long int	resp;
+
+	if (n >= 0)
+		resp = 1;
+	else
+		resp = 0;
+	return (resp);
 }
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	size_t	str;
-	size_t	i;
-	char s;
+	long int	qto_d;
+	long int	nb2;
 
-	str = n;
-	i = ft_lendec(n);
-	if (n < 0)
+	nb2 = n;
+	if (n == 0)
+		write(fd, "0", 1);
+	else if (ft_is_negative(nb2) == 0)
 	{
-		str = (long long)n * -1;
 		write(fd, "-", 1);
+		nb2 = nb2 * (-1);
 	}
-	while (str)
-	{
-		s = (str % 10) + '0';
-		write(fd, &s, 1);
-		str = str / 10;
-	}
+	qto_d = qto_casas(nb2);
+	calc_print(qto_d, nb2, fd);
 }
