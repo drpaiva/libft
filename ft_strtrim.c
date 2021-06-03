a34f7c59c15df1a42927f6a8aff67a4d113e4911
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-06-03 12:54:45 -0300

    ajust strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 03:13:42 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/06/03 12:52:27 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/06/03 12:54:40 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	if (!s1 || set == 0)
 		return (0);
-	while (s1[0] && s1[0] != NULL && ft_strchr(set, s1[0]))
+	while (s1[0] && s1[0] != '\0' && ft_strchr(set, s1[0]))
 		s1++;
 	f = ft_strlen(s1);
 	if (f <= 0)
