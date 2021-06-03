147bdd072ac095245f189c7d0b24fc02673db5a1
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-06-03 12:40:25 -0300

    ft_calloc bug 3

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,18 +6,19 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/22 13:51:17 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/06/03 12:35:15 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/06/03 12:40:17 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-void	*ft_calloc(size_t count, size_t size)
-{
-	char	*r;
 
-	r = malloc(count * size);
-	if (r == NULL)
-		return (NULL);
-	ft_bzero(r, count * size);
-	return (r);
+void    *ft_calloc(size_t count, size_t size)
+{
+    char    *pointer;
+
+    pointer = malloc(count * size);
+    if (pointer == NULL)
+        return (NULL);
+    ft_bzero(pointer, count * size);
+    return (pointer);
 }
