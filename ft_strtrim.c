2d2328f48aa22bf5562e3d5c2ed8b0145879b27c
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-06-03 13:04:41 -0300

    ajust strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 03:13:42 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/06/03 12:58:12 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/06/03 13:03:23 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	if (!s1 || set == 0)
 		return (0);
-	while (s1[0] && s1[0] != '\0' && ft_strchr(set, s1[0]))
+	while (s1[0] != '\0' && ft_strchr(set, s1[0]))
 		s1++;
 	f = ft_strlen(s1);
 	if (f <= 0)
 		return (ft_calloc(sizeof(char), f));
-	f--;
+	// f--;
 	while (s1[f] && ft_strchr(set, s1[f]))
 		f--;
 	res = ft_calloc(sizeof(char), (f + 1));
