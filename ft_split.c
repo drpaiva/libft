92279136031fecd91b9000f379a579dde3e76b56
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-06-04 12:31:14 -0300

    alter split 2

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 19:01:12 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/06/04 12:22:15 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/06/04 12:30:28 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,9 +50,10 @@ static int	ft_strlendelimiter(char const *s, char c)
 		}
 		i++;
 	}
-	if (!len && !s)
-		return (0);
-	return (len + 1);
+	if (s)
+		return (len + 1);
+	return (0);
+	
 }
 
 char	**ft_split(char const *s, char c)
