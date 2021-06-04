99e0ae63c0ed61454a670514137085ab9aa57557
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-06-04 19:42:34 -0300

    teste

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 19:01:12 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/06/04 18:39:11 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/06/04 19:41:50 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,15 +54,13 @@ static int	ft_strlendelimiter(char const *s, char c)
 		}
 		i++;
 	}
-	// if (s)
-	// 	return (len + 1);
 	return (len);
-	
 }
 
 static char	**ft_nalloc(int len)
 {
 	char	**res;
+
 	if (!len)
 	{
 		res = (char **)ft_calloc(len + 1, sizeof(char *));
@@ -77,8 +75,8 @@ static char	**ft_nalloc(int len)
 
 static char	**ft_pop(int *i, char *str, char **res, char c)
 {
-	int len;
-	
+	int	len;
+
 	len = (ft_strlendelimiter(str, c));
 	i[0] = 0;
 	while (len + 1)
@@ -88,6 +86,8 @@ static char	**ft_pop(int *i, char *str, char **res, char c)
 			return (NULL);
 		i[1] = ft_strlencut(str, c);
 		res[i[0]] = ft_substr(str, 0, i[1]);
+		if (!res[i[0]])
+			return (ft_free(res));
 		while (i[1]--)
 			str++;
 		len--;
@@ -114,6 +114,5 @@ char	**ft_split(char const *s, char c)
 	if (!ft_strlen(str))
 		return (res);
 	res = ft_pop(i, str, res, c);
-
 	return (res);
 }
