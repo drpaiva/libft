a5d214fc2c30152104ffb41d8ba69b3582ee4587
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-06-03 22:29:52 -0300

    new split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 19:01:12 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/06/03 17:43:09 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/06/03 22:22:57 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,75 +14,85 @@
 
 static int	ft_strlencut(char *s, char c)
 {
+	char *s1;
 	int	i;
 
+	s1 = ft_strdup(s);
 	i = 0;
 	if (!s)
 		return (0);
-	while (*s != '\0' && *s != c)
+	while (*s1 != '\0' && *s1 != c)
 	{
 		i++;
-		s++;
+		s1++;
 	}
-	return (i++);
+	return (i);
 }
 
-static char	**ft_free(char **s)
-{
-	int	i;
+// static char	**ft_free(char **s)
+// {
+// 	int	i;
 
-	i = 0;
-	while (s[i])
-	{
-		free(s[i]);
-		i++;
-	}
-	free(s);
-	return (NULL);
-}
+// 	i = 0;
+// 	while (s[i])
+// 	{
+// 		free(s[i]);
+// 		i++;
+// 	}
+// 	free(s);
+// 	return (NULL);
+// }
 
 static int	ft_strlendelimiter(char const *s, char c)
 {
 	int	len;
+	int	i;
+	int	j;
+	char *str;
+	char	d[2];
 
+	d[0] = c;
+	d[1] = '\0';
+	str = ft_strdup(s);
+	str = ft_strtrim(str, d);
 	len = 0;
-	while (*s)
+	j = ft_strlen(s);
+	while (i < j)
 	{
-		if (*s != c)
-		{
+		if (s[i] == c)
 			len++;
-			while (*s && *s != c)
-				s++;
-		}
-		s++;
+		i++;
 	}
+	
 	return (len);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**res;
-	char	*ns;
-	int		i[2];
-	char	t[2];
+	char	*str;
+	int		len;
+	int		i[3];
 
-	t[0] = c;
-	t[1] = '\0';
-	ns = ft_strtrim(s, t);
-	res = (char **)ft_calloc(ft_strlendelimiter(ns, t[0]) + 1, sizeof(char *));
+	str = ft_strdup(s);
+	len = (ft_strlendelimiter(str, c) + 1);
+	res = (char **)ft_calloc(len + 1, sizeof(char *));
 	if (!res)
 		return (0);
 	i[0] = 0;
-	while (ft_strlencut(ns, c))
+	while (len)
 	{
-		i[1] = ft_strlencut(ns, c) + 1;
-		res[i[0]] = ft_calloc(i[1], sizeof(char));
-		if (!res[i[0]])
-			return (ft_free(res));
-		ft_strlcpy(res[i[0]], ns, i[1]);
-		ns = ft_strtrim(ft_strchr(ns, c), t);
+		i[1] = ft_strlencut(str, c);
+		res[i[0]] = ft_substr(str, 0, i[1]);
+		while (i[1])
+		{
+			str++;
+			i[1]--;
+		}
+		len--;
+		str++;
 		i[0]++;
 	}
-	res[i[0]] = NULL;
+	res[i[0]++] = NULL;
 	return (res);
 }
