108d1a8fad62c816ffbdd95e8ff5414526e70931
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-06-04 22:53:25 -0300

    add new split

--- ft_split.c
+++ ft_split.c
@@ -5,50 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/26 19:01:12 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/06/04 21:22:16 by dramos-p         ###   ########.fr       */
+/*   Created: 2021/06/04 21:25:32 by dramos-p          #+#    #+#             */
+/*   Updated: 2021/06/04 22:50:56 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_strlencut(char *s, char c)
+static int	ft_lendelimiter(char const *s, char c)
 {
-	char	*s1;
 	int		i;
-
-	while ((unsigned char)s[0] == (unsigned char)c)
-		s++;
-	s1 = ft_strdup(s);
-	i = 0;
-	if (!s || !s1)
-		return (0);
-	while (*s1 != '\0' && *s1 != c)
-	{
-		i++;
-		s1++;
-	}
-	return (i);
-}
-
-static int	ft_strlendelimiter(char const *s, char *nc)
-{
 	int		len;
-	int		i;
-	int		j;
-	char	*str;
+	int		lenfull;
 
-	str = ft_strtrim(s, nc);
-	if (!str)
-		return (0);
-	len = 0;
-	j = ft_strlen(str);
+	lenfull = ft_strlen(s);
 	i = 0;
-	while (i < j)
+	while (i <= lenfull)
 	{
-		if ((unsigned char)str[i] == (unsigned char)nc[0])
+		if ((unsigned char)s[i] != (unsigned char)c)
 		{
-			while ((unsigned char)str[i] == (unsigned char)nc[0])
+			while ((unsigned char)s[i] != (unsigned char)c && s[i])
 				i++;
 			len++;
 		}
@@ -57,71 +33,39 @@ static int	ft_strlendelimiter(char const *s, char *nc)
 	return (len);
 }
 
-static char	**ft_nalloc(int len)
-{
-	char	**res;
-
-	if (!len)
-	{
-		res = (char **)ft_calloc(len + 1, sizeof(char *));
-		return (res);
-	}
-	else
-		res = (char **)ft_calloc(len + 2, sizeof(char *));
-	if (!res)
-		return (NULL);
-	return (res);
-}
-
-static char	**ft_pop(int *i, char *str, char **res, char *nc)
-{
-	int	len;
-
-	len = (ft_strlendelimiter(str, nc));
-	i[0] = 0;
-	while (len + 1)
-	{
-		str = ft_strtrim(str, nc);
-		if (!str)
-			return (NULL);
-		i[1] = ft_strlencut(str, nc[0]);
-		res[i[0]] = ft_substr(str, 0, i[1]);
-		if (!res[i[0]])
-		{
-			while (res[i[2]])
-				free(res[i[2]++]);
-			free(res);
-			return (NULL);
-		}
-		while (i[1]--)
-			str++;
-		len--;
-		str++;
-		i[0]++;
-	}
-	return (res);
-}
-
 char	**ft_split(char const *s, char c)
 {
 	char	**res;
-	char	*str;
-	int		len;
-	int		i[3];
-	char	nc[2];
+	int		lendelimiter;
+	int		lenwords;
+	int		i;
+	int		d;
 
-	i[2] = 0;
-	nc[0] = c;
-	nc[1] = '\0';
-	str = ft_strtrim(s, nc);
-	if (!str)
-		return (NULL);
-	len = (ft_strlendelimiter(str, nc));
-	res = ft_nalloc(len);
+	lendelimiter = ft_lendelimiter(s, c);
+	res = (char **)ft_calloc((lendelimiter + 1), sizeof(char *));
 	if (!res)
 		return (NULL);
-	if (!ft_strlen(str))
-		return (res);
-	res = ft_pop(i, str, res, nc);
+	i = 0;
+	while (s[i])
+	{
+		lenwords = 0;
+		if ((unsigned char)s[0] != (unsigned char)c)
+		{
+			while ((unsigned char)s[lenwords] != (unsigned char)c && s[lenwords])
+			{
+				lenwords++;
+			}
+			res[i] = (char *)ft_calloc(lenwords + 1, sizeof(char));
+			if (!res[i])
+				return (NULL);
+			d = 0;
+			while (d <= lenwords)
+			{
+				res[i][d] = *s++;
+				d++;
+			}
+		}
+		i++;
+	}
 	return (res);
 }
