aa481c1c31383aa028605782ba868717590076fa
Diego Ramos Paiva <diego.ramos1@gmail.com>
2021-06-09 01:12:19 -0300

    add protect funtcion ft_lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: dramos-p <dramos-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 15:41:01 by dramos-p          #+#    #+#             */
-/*   Updated: 2021/06/09 01:07:20 by dramos-p         ###   ########.fr       */
+/*   Updated: 2021/06/09 01:11:57 by dramos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,13 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*p;
+	t_list *p;
 
-	if (lst == NULL || new == NULL)
-		return ;
-	if (*lst == NULL)
-	{
-		*lst = new;
-		return ;
-	}
-	else
+    if (*lst)
 	{
 		p = ft_lstlast(*lst);
-		new->next = p->next;
 		p->next = new;
 	}
+	else
+		*lst = new;
 }
